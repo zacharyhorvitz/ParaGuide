93cfd79c12ebe390770d2c0fdf437c9ee3b97a43
Zachary Horvitz <zacharyhorvitz@users.noreply.github.com>
2024-03-07 00:59:32 -0500

    Update download_training_dataset.sh

--- data/enron/download_training_dataset.sh
+++ data/enron/download_training_dataset.sh
@@ -6,6 +6,6 @@ DATA_DOWNLOAD_DIR='/mnt/swordfish-pool2/horvitz/paraphrase_data/ENRON/shards/hol
 
 # gdown https://drive.google.com/file/d/1-IGVQDMRG9vhR9vgQcqUsg0oo36f9e3H/view?usp=sharing --fuzzy -O ${DATA_DOWNLOAD_DIR}/enron_data.zip
 
-# unzip $DATA_DOWNLOAD_DIR/enron_data.zip -d $DATA_DIR
+# unzip $DATA_DOWNLOAD_DIR/enron_data.zip -d $DATA_DOWNLOAD_DIR
 
-ln -s ${DATA_DOWNLOAD_DIR}/2023-06-22-23.15.35 ${DATA_DIR}/2023-06-22-23.15.35  # if using a different preferred location, you may want to create a symlink to the model directory
+# ln -s ${DATA_DOWNLOAD_DIR}/2023-06-22-23.15.35 ${DATA_DIR}/2023-06-22-23.15.35  # if using a different preferred location, you may want to create a symlink to the model directory
