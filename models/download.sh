2c0a0cacbb4f840dedeab6e040b4bc43bf235c16
Zachary Horvitz <zacharyhorvitz@gmail.com>
2024-03-17 16:37:57 -0400

    added download logic

--- models/download.sh
+++ models/download.sh
@@ -7,6 +7,10 @@ MODEL_DOWNLOAD_DIR='/burg/nlp/users/zfh2000/enron_model' #"$(dirname "$0")" # Fe
 
 #gdown https://drive.google.com/file/d/1qlIVBGnvjrqxHvFdp0-5WNwZb6iM3pCA/view?usp=sharing --fuzzy -O ${MODEL_DOWNLOAD_DIR}/enron_model.zip
 
-#unzip $MODEL_DOWNLOAD_DIR/enron_model.zip -d $MODEL_DOWNLOAD_DIR
+gdown https://drive.google.com/file/d/1ijQxv5NjBJgnpwaVoqn49GUSISZ32S_T/view?usp=sharing --fuzzy -O ${MODEL_DOWNLOAD_DIR}/enron_model_luar_conditioned.zip
 
-ln -s ${MODEL_DOWNLOAD_DIR}/best_checkpoint ${MODEL_DIR}/best_checkpoint  # if using a different preferred location, you may want to create a symlink to the model directory
+#unzip $MODEL_DOWNLOAD_DIR/enron_model.zip -d $MODEL_DOWNLOAD_DIR
+unzip $MODEL_DOWNLOAD_DIR/enron_model_luar_conditioned.zip -d $MODEL_DOWNLOAD_DIR
+
+#ln -s ${MODEL_DOWNLOAD_DIR}/best_checkpoint ${MODEL_DIR}/best_checkpoint  # if using a different preferred location, you may want to create a symlink to the model directory
+ln -s ${MODEL_DOWNLOAD_DIR}/best_checkpoint_luar_conditioned ${MODEL_DIR}/best_checkpoint_luar_conditioned  # if using a different preferred location, you may want to create a symlink to the model directory
